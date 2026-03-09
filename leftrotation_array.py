b8ddd8028f82d870867937e39baca6de88b0960c
Rohit Jaiswal <jaiswalrohit2004@gmail.com>
2026-03-10 03:08:55 +0530

    hackerrank: Left Rotation  Array Question  code

--- leftrotation_array.py
+++ leftrotation_array.py
@@ -5,12 +5,24 @@
 # new[i] = old[(i+d)%n] vs new[(i-d)%n] = old[i]
 # approaches to solve: Brute force, Index mapping, Slicing, reversal algorithm
 
+# Wrap-around example
+# Using arr=[1,2,3,4,5], d=2, n=5
+#Let’s fill each result[i]:
+
+#i=0 → (0+2)%5 = 2 → arr[2]=3 → result[0]=3
+#i=1 → (1+2)%5 = 3 → arr[3]=4 → result[1]=4
+#i=2 → (2+2)%5 = 4 → arr[4]=5 → result[2]=5
+#i=3 → (3+2)%5 = 0 → arr[0]=1 → result[3]=1 ✅ wrapped!
+#i=4 → (4+2)%5 = 1 → arr[1]=2 → result[4]=2 ✅ wrapped!
+#Final:
+#result = [3,4,5,1,2]
+
 
 # index mapping approach
 def rotateLeft(d, arr):
     n = len(arr)
     d %= n
-    result = [0] * n
+    result = [0] * n     # Creates an Array with zero values
     for i in range(n):
         result[i] = arr[(i + d) % n]
     return result
